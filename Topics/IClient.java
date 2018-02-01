f6e1b003834a2e1c308781927322c0f43858a5b8
Tim Givois <tim.givois.mendez@gmail.com>
2018-01-31 21:21:21 -0600

    getName should throw exception

--- Topics/IClient.java
+++ Topics/IClient.java
@@ -2,7 +2,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface IClient extends java.rmi.Remote {
-  String getName();
+  String getName() throws RemoteException;;
   void receiveEnter(String name) throws RemoteException;
   void receiveExit(String name)throws RemoteException;
   void receiveMessage(Message message) throws RemoteException;
